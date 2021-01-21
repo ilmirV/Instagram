4e8a6506c3cd6cac6f072d8d7fbe099456b46e99
Freddy Elisabeth <felisabe@ge-i6.kzn.21-school.ru>
2021-01-21 11:05:40 +0300

    First commit

---
+++ Instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Instagram
+//
+//  Created by Freddy Elisabeth on 1/21/21.
+//  Copyright © 2021 Freddy Elisabeth. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
